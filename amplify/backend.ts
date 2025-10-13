60ff7d36fef3a3e335b010d7825a0bf445e3d750
Francois Leduc <fleduc.perso@gmail.com>
2025-10-13 09:50:03 -0400

    adding serverless backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,4 +1,5 @@
 import { defineBackend } from '@aws-amplify/backend';
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
@@ -9,3 +10,24 @@ defineBackend({
   auth,
   data,
 });
+
+const bedrockDataSource = backend.data.resources.graphqlApi.addHttpDataSource(
+    "bedrockDS",
+    "https://bedrock-runtime.us-east-1.amazonaws.com",
+    {
+      authorizationConfig: {
+        signingRegion: "us-east-1",
+        signingServiceName: "bedrock",
+      },
+    }
+);
+
+bedrockDataSource.grantPrincipal.addToPrincipalPolicy(
+    new PolicyStatement({
+      resources: [
+        "arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0",
+      ],
+      actions: ["bedrock:InvokeModel"],
+
+    })
+);
